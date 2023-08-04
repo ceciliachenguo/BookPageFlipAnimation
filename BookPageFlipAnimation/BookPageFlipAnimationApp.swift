009cc8112a11fc29aead459881cca8ab18194910
CeciliaChen <cecilia@pdotf.com>
2023-08-04 15:02:24 -0400

    Initial Commit

---
+++ BookPageFlipAnimation/BookPageFlipAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookPageFlipAnimationApp.swift
+//  BookPageFlipAnimation
+//
+//  Created by Cecilia Chen on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct BookPageFlipAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookPageFlipAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookPageFlipAnimation
+//
+//  Created by Cecilia Chen on 8/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
